70ae121b056a6417ce98eeac67815283c3dfa721
gdotta30 <gdotta30@gmail.com>
2018-06-09 16:20:07 -0300

    All

---
+++ lib/build/generated-sources/ap-source-output/pracUno/Cliente_.java
@@ -0,0 +1,16 @@
+package pracUno;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-09T16:11:53")
+@StaticMetamodel(Cliente.class)
+public class Cliente_ { 
+
+    public static volatile SingularAttribute<Cliente, String> firstName;
+    public static volatile SingularAttribute<Cliente, String> lastName;
+    public static volatile SingularAttribute<Cliente, Integer> idCliente;
+    public static volatile SingularAttribute<Cliente, String> email;
+
+}
