08b9a730e6c0998b26dc4bdbf2303516285c586e
jcarbajales <jose_carbajales@hotmail.com>
2018-06-13 17:54:19 -0300

    Pagina Home para ingresar y registrar. Luego pagina del usuario para Agregar Noticias.

---
+++ src/java/interfaces/Noticia.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author jose_
+ */
+public class Noticia {
+    
+    
+    private String usuario;
+    private String titulo;
+    private String validacion;
+        
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * @param titulo the titulo to set
+     */
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    /**
+     * @return the validacion
+     */
+    public String getValidacion() {
+        return validacion;
+    }
+
+    /**
+     * @param validacion the validacion to set
+     */
+    public void setValidacion(String validacion) {
+        this.validacion = validacion;
+    }
+    
+    
+    
+}
